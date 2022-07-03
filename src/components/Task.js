f05131388e0d0318add82c0a2728fc58a7376b65
alialaba <alialaba079@gmail.com>
2022-07-03 18:32:24 -0400

    bug:fix bug

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,7 +3,7 @@ const Task =({task, onDelete})=>{
     return(
         <div className="task" >
             <h3>{task.text}
-            //task.id identify the specify task to delete
+            
             <FaTimes style={{color:"red"}} onClick={()=> onDelete(task.id)}/>
             </h3>
             <p>{task.day}</p>
